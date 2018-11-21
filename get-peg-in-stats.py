f49e3a7ab639b4e0b2134cd675d7d1c931b25279
Allen Piscitello <allen.piscitello@gmail.com>
2018-11-21 09:33:51 -0600

    Moving saving progress to database

--- get-peg-in-stats.py
+++ get-peg-in-stats.py
@@ -11,11 +11,9 @@ def log_downtime(configuration, block_time):
         while block_time > configuration.expected_block_time:
             functionary = LiquidConstants.get_functionary_by_minute(configuration.expected_block_time.minute)
             configuration.logger.log_missed_block(configuration.expected_block_time, functionary)
-            configuration.stats.missed_rounds[functionary - 1] = configuration.stats.missed_rounds[functionary - 1] + 1
             configuration.expected_block_time = configuration.expected_block_time + timedelta(seconds=60)
             downtime = downtime + 1
     if downtime > 15:
-        configuration.stats.total_downtime = configuration.stats.total_downtime + downtime
         configuration.logger.log_downtime(block_time, downtime)
 
 def log_inputs(configuration, tx_full, block_time, block_height):
@@ -23,7 +21,6 @@ def log_inputs(configuration, tx_full, block_time, block_height):
         if "is_pegin" in input and input["is_pegin"]:
             mainchain = get_bitcoin_rpc().decoderawtransaction(input["pegin_witness"][4])
             configuration.logger.log_peg(block_height, block_time, int(decimal.Decimal(10e8) * mainchain["vout"][input["vout"]]["value"]))
-            configuration.stats.amount_in = configuration.stats.amount_in + mainchain["vout"][input["vout"]]["value"]
         if "issuance" in input:
             issuance = input["issuance"]
             if "assetamount" in issuance:          
@@ -35,27 +32,10 @@ def log_outputs(configuration, tx_full, block_time, block_height):
      for output in tx_full["vout"]:
         if "pegout_chain" in output["scriptPubKey"]:
             configuration.logger.log_peg(block_height, block_time, int(decimal.Decimal(10e8)*(0-output["value"])))
-            configuration.stats.amount_in = configuration.stats.amount_in - output["value"]
         if "addresses" in output["scriptPubKey"] and output["scriptPubKey"]["addresses"][0] == LiquidConstants.liquid_fee_address:
-            configuration.stats.fee_total = configuration.stats.fee_total + output["value"]
             configuration.logger.log_fee(block_height, block_time, int(decimal.Decimal(10e8) * output["value"]))
         if output["scriptPubKey"]["asm"] == "OP_RETURN" and "asset" in output and output["asset"] != LiquidConstants.btc_asset and "value" in output and output["value"] > 0:
             configuration.logger.log_issuance(block_height, block_time, output["asset"], int(decimal.Decimal(10e8) * 0-output["value"]))       
-
-def print_stats(stats, block_height, last_block_time):
-    print "amount in : {0}".format(stats.amount_in)
-    print "fees collected: {0}".format(stats.fee_total)
-    print "missed round count: {0}".format(stats.missed_rounds)
-    print "total downtime: {0}".format(stats.total_downtime)
-    print "last block: {0}".format(block_height)
-    print "last block time: {0}".format(last_block_time)
-
-def write_headers(peg_writer, fee_writer, issuance_writer, outages_writer, missing_rounds_writer):
-    peg_writer.writerow(['Direction', 'Block','Date', 'Amount'])
-    fee_writer.writerow(['Block','Date', 'Amount'])
-    issuance_writer.writerow(['Block','Date', 'Asset', 'Amount'])
-    outages_writer.writerow(['Outage End','Length'])
-    missing_rounds_writer.writerow(['Missing Time', 'Functionary Id'])
 
 def main():
     configuration = Configuration()
@@ -73,8 +53,7 @@ def main():
 
             if i % 1000 == 0:
                 print "Block {0}".format(i)
-                print_stats(configuration.stats, i, block_time)
-                configuration.logger.conn.commit()
+                configuration.save(i, block_time)
 
             log_downtime(configuration, block_time)
             liquid_rpc = get_liquid_rpc()
@@ -83,10 +62,9 @@ def main():
                 log_inputs(configuration, tx_full, block_time, i)
                 log_outputs(configuration, tx_full, block_time, i)
             configuration.expected_block_time = block_time + timedelta(seconds=60)
-
+            
         configuration.save(last_height, block_time)
-        print_stats(configuration.stats, i, block_time)
-        configuration.logger.conn.commit()
+        print "Complete at block {0}".format(last_height)
     else:
         print "Nothing new to parse."
 
--- logger.py
+++ logger.py
@@ -5,7 +5,7 @@ def to_timestamp(time_as_datetime):
     return int((time_as_datetime - datetime.fromtimestamp(0)).total_seconds())
 
 class Logger:
-    def __init__(self, start_time= None):
+    def __init__(self):
         #Initialize Database if not created
         self.conn = sqlite3.connect('liquid.db')
         self.conn.execute('''CREATE TABLE if not exists missing_blocks (datetime int, functionary int)''')
@@ -13,20 +13,25 @@ class Logger:
         self.conn.execute('''CREATE TABLE if not exists outages (end_time int, length int)''')
         self.conn.execute('''CREATE TABLE if not exists pegs (block int, datetime int, amount int)''')
         self.conn.execute('''CREATE TABLE if not exists issuances (datetime int, block int, asset text, amount int NULL)''')
+        self.conn.execute('''CREATE TABLE if not exists last_block (block int, datetime int)''')
 
-        if start_time == None:
+        configuration = self.conn.execute("SELECT block, datetime FROM last_block").fetchall()
+        if len(configuration) == 0:      
             self.conn.execute('''DELETE FROM missing_blocks''')
             self.conn.execute('''DELETE FROM fees''')
             self.conn.execute('''DELETE FROM outages''')
             self.conn.execute('''DELETE FROM pegs''')
             self.conn.execute('''DELETE FROM issuances''')
+            self.last_block = 0
+            self.last_time = None
         else:
-            self.conn.execute('''DELETE FROM missing_blocks WHERE datetime >= ? ''', (to_timestamp(start_time),))
-            self.conn.execute('''DELETE FROM fees WHERE datetime >= ? ''', (to_timestamp(start_time),))
-            self.conn.execute('''DELETE FROM outages WHERE end_time >= ? ''', (to_timestamp(start_time),))
-            self.conn.execute('''DELETE FROM pegs WHERE datetime >= ? ''', (to_timestamp(start_time),))
-            self.conn.execute('''DELETE FROM issuances WHERE datetime >= ? ''', (to_timestamp(start_time),))
-        
+            self.last_time = datetime.fromtimestamp(configuration[0][1])
+            self.last_block = configuration[0][0]
+            self.conn.execute('''DELETE FROM missing_blocks WHERE datetime >= ? ''', (to_timestamp(self.last_time),))
+            self.conn.execute('''DELETE FROM fees WHERE datetime >= ? ''', (to_timestamp(self.last_time),))
+            self.conn.execute('''DELETE FROM outages WHERE end_time >= ? ''', (to_timestamp(self.last_time),))
+            self.conn.execute('''DELETE FROM pegs WHERE datetime >= ? ''', (to_timestamp(self.last_time),))
+            self.conn.execute('''DELETE FROM issuances WHERE datetime >= ? ''', (to_timestamp(self.last_time),)) 
         self.conn.commit()
 
     def log_issuance(self, block_height, block_time, asset_id, amount):
@@ -45,4 +50,7 @@ class Logger:
     def log_missed_block(self, expected_block_time, functionary):
         self.conn.execute("INSERT INTO missing_blocks VALUES (?, ?)", (to_timestamp(expected_block_time), functionary))
         
-
+    def save_progress(self, last_block, last_timestamp):
+        self.conn.execute("DELETE FROM last_block")
+        self.conn.execute("INSERT INTO last_block VALUES (?, ?) ", (last_block, to_timestamp(last_timestamp)))
+        self.conn.commit()
